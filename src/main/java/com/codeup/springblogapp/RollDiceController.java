9a1ffc59024da6a2913b75ff78ebad1e798439de
Mitchel Sandidge <masandidge12@gmail.com>
2020-05-11 19:55:27 -0500

    Finished Dice-roll exercise

--- src/main/java/com/codeup/springblogapp/RollDiceController.java
+++ src/main/java/com/codeup/springblogapp/RollDiceController.java
@@ -1,46 +1,57 @@
 package com.codeup.springblogapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
 
 @Controller
 public class RollDiceController {
 
+    // TODO: 1. a. Create a page at /roll-dice that asks the user to guess a number.
+    //          b. There should be links on this page for 1 through 6 that should make a GET request to /roll-dice/n where n is the number guessed.
+    //          c. This page should display a random number (the dice roll), the user's guess and a message based on whether or not they guessed the correct number.
+    //          Bonus
+    //          "Roll" a series of dice on each page load, as opposed to an individual die. Show all the rolls to the user and display how many rolls matched their guess.
+
     @GetMapping("/roll-dice")
-    public String welcome() {
-        return "roll-dice";
+    public String showRollDice(Model model){
+
+        model.addAttribute("hasRolled", false); // The user has not rolled yet
+
+        return "roll-dice"; // we return the string "Roll-dice" from our controller, Spring will try to find a file name roll-dice.html inside of resources/templates.
+
     }
 
+    @GetMapping("/roll-dice/{n}") // n is the number guessed by user
+    public String showUserSelection(@PathVariable int n, Model model){ // A Model in this context is what Spring refers to a view as
 
-    // Different Pages for the DICE
-    @GetMapping("/roll-dice/1")
-    public String one() {
-        return "1";
-    }
+        // create instance of Random class
+        Random random = new Random();
+        // Generate random integers in range 0 to 6 (6 sided die)
+        int randomInt = random.nextInt(6);
 
-    @GetMapping("/roll-dice/2")
-    public String two() {
-        return "2";
-    }
+        // 1. Once the user has rolled
+        model.addAttribute("hasRolled", true); // The user has rolled
+        // 2. Show the random number
+        String randomDisplay = randomInt + " was rolled.";
+        model.addAttribute("randomNumber", randomDisplay);
+        // 3. Display the users guess
+        String guessDisplay = n + " was your guess.";
+        model.addAttribute("userGuess", guessDisplay);
+        // 4. Check if guess and random number match
+        // 5. Tell the user if they guessed correct or incorrect
+        if (randomInt == n)
+            model.addAttribute("message", "NICE! Your guess was correct!");
+        else
+            model.addAttribute("message", "Oops :( Your guess was incorrect!");
 
-    @GetMapping("/roll-dice/3")
-    public String three() {
-        return "3";
-    }
 
-    @GetMapping("/roll-dice/4")
-    public String four() {
-        return "4";
-    }
-
-    @GetMapping("/roll-dice/5")
-    public String five() {
-        return "5";
-    }
-
-    @GetMapping("/roll-dice/6")
-    public String six() {
-        return "6";
+        return "roll-dice"; // we return the string "Roll-dice" from our controller, Spring will try to find a file name roll-dice.html inside of resources/templates.
     }
 
 }
