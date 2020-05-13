90b20d4fde6b5e1a47b804a5791e89d76eef6995
Mitchel Sandidge <masandidge12@gmail.com>
2020-05-13 17:40:04 -0500

    Added Edit functionallity to the Posts

--- src/main/java/com/codeup/springblogapp/controllers/PostController.java
+++ src/main/java/com/codeup/springblogapp/controllers/PostController.java
@@ -4,10 +4,7 @@ import com.codeup.springblogapp.PostRepository;
 import com.codeup.springblogapp.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,6 +52,43 @@ public class PostController {
         model.addAttribute("post", postDao.getPostById(id));
         return "/posts/show";
     }
+
+
+
+
+
+
+
+    //    Edit a post
+    @GetMapping("/posts/edit/{id}")
+    public String postEditForm(@PathVariable long id, Model model) {
+        Post post = postDao.getPostById(id);
+        model.addAttribute("post", post);
+        return "/posts/edit";
+    }
+    @PostMapping("/posts/edit/{id}")
+    public String postEdit(@PathVariable long id, @RequestParam(name = "title") String title, @RequestParam(name = "body") String body) {
+        Post post = postDao.getPostById(id);
+        post.setTitle(title);
+        post.setBody(body);
+        postDao.save(post);
+        return "redirect:/posts";
+    }
+
+
+
+//    //    <-----EDIT Post FORM----->
+//    @GetMapping("/posts/edit/{id}")
+//    public String viewEditForm(@PathVariable long id, Model model) {
+//
+//        model.addAttribute("post", postDao.getPostById(id));
+//
+//
+//        return "/posts/edit";
+//    }
+
+
+
 
 
     //    <-----DELETE Post----->
